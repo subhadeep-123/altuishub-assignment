95052348d2e89d8d516a1f32fd0d5b39ee0ae54c
Subhadeep Banerjee <subhadeep@klizos.com>
2025-06-26 11:27:18 +0530

    Add models for the application

--- application/backend/productCatalog/core/models.py
+++ application/backend/productCatalog/core/models.py
@@ -1,3 +1,26 @@
+from django.contrib.gis.db import models as geomodels
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Apartment(models.Model):
+    BEDROOM_CHOICES = [
+        ("1BHK", "1 Bedroom"),
+        ("2BHK", "2 Bedroom"),
+        ("3BHK", "3 Bedroom"),
+    ]
+    title = models.CharField(max_length=255)
+    price = models.IntegerField()
+    bedroom_type = models.CharField(choices=BEDROOM_CHOICES, max_length=5)
+    location = geomodels.PointField(geography=True)  # Longitude/Latitude
+    listed_by = models.ForeignKey(User, on_delete=models.CASCADE)
