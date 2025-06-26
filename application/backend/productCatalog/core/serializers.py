2b53fd0d4fe50807cf17b9ac5f2e1aee67ee3f9c
Subhadeep Banerjee <subhadeep@klizos.com>
2025-06-26 11:33:07 +0530

    Add serializers for model serialization

---
+++ application/backend/productCatalog/core/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .model import User, Product, Apartment
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class ApartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Apartment
+        fields = "__all__"
